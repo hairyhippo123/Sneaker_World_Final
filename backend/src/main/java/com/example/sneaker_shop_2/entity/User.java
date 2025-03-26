d7e8bfa6dd06d00f7f40c4a6ecf0ac5afae29143
hairyhippo123 <nguyenquyet2003hq@gmail.com>
2025-03-26 07:31:56 +0700

    Fix Bug

--- backend/src/main/java/com/example/sneaker_shop_2/entity/User.java
+++ backend/src/main/java/com/example/sneaker_shop_2/entity/User.java
@@ -54,7 +54,9 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles = new HashSet<>();
+    public User(){
 
+    }
     public User(Integer userId) {
         this.userId = userId;
     }
