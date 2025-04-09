96807257a491fdfc6523c3ba0e25a23f85775561
hairyhippo123 <nguyenquyet2003hq@gmail.com>
2025-04-09 14:50:33 +0700

    testLogin

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -27,7 +27,7 @@ const Signup = () => {
             <div className="auth-box">
                 <div className="auth-header">
                     <h1 className="logo" onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
-                        Seazyyy
+                        Sneaker World
                     </h1>
                 </div>
                 <div className="auth-tabs">
